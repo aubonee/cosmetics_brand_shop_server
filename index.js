54cf2e2021a155ea55f26ead27f2799dfbc22514
aubonee <chytahsin.2210@gmail.com>
2023-10-19 03:09:52 +0600

    data get done successfully

--- index.js
+++ index.js
@@ -37,13 +37,25 @@ async function run() {
     
     const productCollection =client.db('productDB').collection('product');
    
-    app.get('/products', async(req,res)=>{
-      const cursor =productCollection.find();
+  //  app.get('/products', async(req,res)=>{
+  //     app.get('/products', async(req,res)=>{
+  //     const cursor =productCollection.find();
      
-      const result =await cursor.toArray();
-      res.send(result);
+  //     const result =await cursor.toArray();
+  //     res.send(result);
 
-    })
+  //   })
+
+  //app.get('/products', async(req,res)=>{
+    app.get('/products', async(req,res)=>{
+    const cursor =productCollection.find();
+   
+    const result =await cursor.toArray();
+    res.send(result);
+
+  })
+
+
 
     app.post('/products', async(req,res)=>{
       
