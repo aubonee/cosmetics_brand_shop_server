65ab2753c04a398fdb58f58f96a5e53e9cb20e65
aubonee <chytahsin.2210@gmail.com>
2023-10-18 11:14:15 +0600

    gitignore added

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -7,18 +8,18 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(cors());
 app.use(express.json());
-//CosmeticsProduct
-//hIlLyKVhhYrlRdMz
 
 
 
-//cosmeticsBrandSite
-//TyA8MfirbxR2rjbk
+//chytahsin2210
+//NDtSgKecnzt11XU8
 
-//cosmeticsItems
-//RYiF6X4ndvjbo7Dn
-//const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://cosmeticsItems:RYiF6X4ndvjbo7Dn@cluster0.9lrecxe.mongodb.net/?retryWrites=true&w=majority";
+//console.log(process.env.DB_USER)
+//console.log(process.env.DB_PASS)
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jkbvpzz.mongodb.net/?retryWrites=true&w=majority`;
+
+
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -38,11 +39,10 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
-
 
 
 
